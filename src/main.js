df1f60f34501837d65d7f03edf918a81e57a11cf
Preetam Kajal Rout <preetamkajalrout@gmail.com>
2019-10-01 20:26:16 +0530

    [FEAT] Adds skelton for destination handling

--- src/main.js
+++ src/main.js
@@ -14,17 +14,72 @@
  * @returns {function} Middleware function to use
  */
 module.exports = function({resources, options}) {
-  let destinations;
-  let routes;
-  let routesConfig;
+  const destinations = require(options.configuration.destination_path)["destinations"];
+  const routes = require(resources.rootProject._readers[0]._project.path + "/neo-app.json")["routes"];
   const httpProxy = require("http-proxy");
   const proxy = new httpProxy.createProxyServer();
 
+  let routesConfig = {};
+
+  function buildRouteConfig () {
+      // Build a object map for each route
+      routes.forEach(function(route) {
+        routesConfig[route.path] = route;
+      });
+  }
+
+  function findRoute (routeToMatch) {
+    let sMatchedRoute = "";
+    // Find the longest matched route
+    for (let route in routesConfig) {
+      if (routeToMatch.includes(route)) {
+        sMatchedRoute = sMatchedRoute ? (sMatchedRoute.length < route.length ? route : sMatchedRoute) : route;
+      }
+    }
+    return sMatchedRoute;
+  }
+
+  function buildProxyObject (reqUrl, matchedRoute) {
+    let routeProxyObject = {};
+    let resourceRoute = routesConfig[matchedRoute].target;
+    let oDestination = destinations[resourceRoute.name];
+    console.log(`Resource: ${reqUrl}`);
+    // If route match found, build the final proxy object to pass around to proxy server
+
+    // Final Target URL for the resource
+    let routeRegExp = new RegExp(`${matchedRoute}(/.*)$`);
+    let resourcePath = routeRegExp.exec(reqUrl)[1];
+
+
+    reqUrl = "";
+    // Serve SAPUI5 resources from cdn or local server or fs
+    if (resourceRoute.type && resourceRoute.type === "service" && resourceRoute.name === "sapui5") {
+      reqUrl = resourceRoute.version ? `/${resourceRoute.version}` : "";
+      routeProxyObject.target = oDestination.cdn;
+    } else {
+      debugger;
+      routeProxyObject.target = oDestination.URL;
+    }
+    reqUrl += resourceRoute.entryPath + resourcePath;
+
+    // Add Authorization info for proxy
+    if (oDestination.User) {
+      routeProxyObject.auth = `${oDestination.User}:${oDestination.Password}`;
+    }
+
+    return {
+      resourcePath: reqUrl,
+      proxyObject: routeProxyObject
+    };
+  }
+
+  buildRouteConfig();
+
   return function (req, res, next) {
       console.log(`[PROXY_SRV] ${req.url}`);
-      let config = options.configuration;
       let sMatchedRoute = "";
       let oMatchedRoute = {};
+      let oRouteProxy = {};
 
       res.header('Access-Control-Allow-Origin', '*');
       res.header('Access-Control-Allow-Credentials', 'true');
@@ -38,31 +93,18 @@ module.exports = function({resources, options}) {
         return;
       }
 
-      destinations = require(config.destination_path)["destinations"];
-      routes = require(resources.rootProject._readers[0]._project.path + "/neo-app.json")["routes"];
-      routesConfig  = {};
-      // Build a object map for each route
-      routes.forEach(function(route) {
-        routesConfig[route.path] = route;
-      });
+      sMatchedRoute = findRoute(req.url);
 
-      // Find the longest matched route
-      for (let route in routesConfig) {
-        if (req.url.includes(route)) {
-          sMatchedRoute = sMatchedRoute ? (sMatchedRoute.length < route ? route : sMatchedRoute) : route;
-        }
-      }
-
-      if (!sMatchedRoute) {  // If no route found
+      if (!sMatchedRoute || sMatchedRoute === "/") {  // If no route found, "/" is found move to next
         next();
         return;
       }
-      console.log("Redirecting: " + destinations[routesConfig[sMatchedRoute].target.name].URL + req.url);
-      // If route match found, build the final proxy object to pass around to proxy server
-      oMatchedRoute.target = destinations[routesConfig[sMatchedRoute].target.name].URL + req.url;
-      if (destinations[routesConfig[sMatchedRoute].target.name].User) {
-        oMatchedRoute.auth = destinations[routesConfig[sMatchedRoute].target.name].User + ":" + destinations[routesConfig[sMatchedRoute].target.name].Password;
-      }
+
+      oRouteProxy = buildProxyObject(req.url, sMatchedRoute);
+      oMatchedRoute = oRouteProxy.proxyObject;
+      req.url = oRouteProxy.resourcePath;
+
+      console.log(`Redirected: ${oMatchedRoute.target}, ${req.url}`);
       proxy.web(req, res, oMatchedRoute, (err) => {
         if (err) {
           next(err);
