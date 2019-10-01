3624d0e362a1981bc90fcbeb5aec762d7a18a8a6
Preetam Kajal Rout <preetamkajalrout@gmail.com>
2019-10-01 22:54:05 +0530

    [FIX] Add clearn structure for proxy pattern

--- src/main.js
+++ src/main.js
@@ -1,3 +1,82 @@
+const log = require("@ui5/logger").getLogger("server:middleware:ui5-middlware-destination");
+const parseurl = require("parseurl");
+const querystring = require("querystring");
+const httpProxy = require("http-proxy");
+const proxyServer = new httpProxy.createProxyServer();
+
+// Global response header constants
+const HEADER_ALLOW_CONTROL_ALLOW_ORIGIN = "Access-Control-Allow-Origin";
+const HEADER_ALLOW_CONTROL_ALLOW_CREDENTIALS = "Access-Control-Allow-Credentials";
+const HEADER_ALLOW_CONTROL_ALLOW_METHODS = "Access-Control-Allow-Methods";
+const HEADER_ALLOW_CONTROL_ALLOW_HEADERS = "Access-Control-Allow-Headers";
+
+// Variables for each app
+var destinations, routes, routesConfig = {};
+
+function addHeader(res, header, value) {
+	const current = res.get(header);
+	if ( current == null ) {
+		res.set(header, value);
+	} else if ( Array.isArray(current) ) {
+		res.set(header, [...current, value]);
+	} else {
+		res.set(header, [current, value]);
+	}
+}
+
+function initProxyDestination () {
+  buildRouteConfigMap();
+}
+
+function buildRouteConfigMap () {
+  routes.forEach(route => {
+    this[route.path] = route;
+  }, routesConfig);
+}
+
+function findRoute (routeToMatch) {
+  let matchedRoute = "";
+
+  // Find the longest matched route
+  for (let route in routesConfig) {
+    if (routeToMatch.includes(route)) {
+      matchedRoute = matchedRoute ? (matchedRoute.length < route.length ? route : matchedRoute) : route;
+    }
+  }
+
+  return matchedRoute;
+}
+
+function buildProxyObject (reqUrl, matchedRoute) {
+  let routeProxyObj = {
+    resourcePath: "",
+    proxyConfig: {}
+  },
+  resourceRoute = routesConfig[matchedRoute].target,
+  routeDestination = destinations[resourceRoute.name],
+  rResourcePattern = new RegExp(`${matchedRoute}(/.*)$`),
+  resourcePath = rResourcePattern.exec(reqUrl)[1]; // Assumes there is some path after provided route url
+
+  // Serve SAPUI5 resources from destinations configuration
+  if (resourceRoute.type && resourceRoute.type === "service" && resourceRoute.name === "sapui5") {
+    routeProxyObj.resourcePath = resourceRoute.version ? `/${resourceRoute.version}` : "",
+    routeProxyObj.proxyConfig.target = routeDestination.cdn;
+  } else {
+    routeProxyObj.resourcePath = "";
+    routeProxyObj.proxyConfig.target = routeDestination.URL;
+  }
+
+  routeProxyObj.proxyConfig.resourcePath += (resourceRoute.entryPath || matchedRoute) + resourcePath;
+
+  // Add authorization info for proxy
+  if (routeDestination.User) {
+    routeProxyObj.proxyConfig.auth = `${routeDestination.User}:${routeDestination.Password}`;
+  }
+
+  return routeProxyObj;
+}
+
+
 /**
  * Custom UI5 Server middleware example
  *
@@ -13,102 +92,50 @@
  * @param {string} [parameters.options.configuration] Custom server middleware configuration if given in ui5.yaml
  * @returns {function} Middleware function to use
  */
-module.exports = function({resources, options}) {
-  const destinations = require(options.configuration.destination_path)["destinations"];
-  const routes = require(resources.rootProject._readers[0]._project.path + "/neo-app.json")["routes"];
-  const httpProxy = require("http-proxy");
-  const proxy = new httpProxy.createProxyServer();
+function createMiddleware({resources, options}) {
+  destinations = require(options.configuration.destination_path)["destinations"];
+  routes = require(resources.rootProject._readers[0]._project.path + "/neo-app.json")["routes"];
 
-  let routesConfig = {};
+  initProxyDestination(); //Build all the configs for upcoming requests
 
-  function buildRouteConfig () {
-      // Build a object map for each route
-      routes.forEach(function(route) {
-        routesConfig[route.path] = route;
-      });
-  }
-
-  function findRoute (routeToMatch) {
-    let sMatchedRoute = "";
-    // Find the longest matched route
-    for (let route in routesConfig) {
-      if (routeToMatch.includes(route)) {
-        sMatchedRoute = sMatchedRoute ? (sMatchedRoute.length < route.length ? route : sMatchedRoute) : route;
-      }
-    }
-    return sMatchedRoute;
-  }
-
-  function buildProxyObject (reqUrl, matchedRoute) {
-    let routeProxyObject = {};
-    let resourceRoute = routesConfig[matchedRoute].target;
-    let oDestination = destinations[resourceRoute.name];
-    console.log(`Resource: ${reqUrl}`);
-    // If route match found, build the final proxy object to pass around to proxy server
-
-    // Final Target URL for the resource
-    let routeRegExp = new RegExp(`${matchedRoute}(/.*)$`);
-    let resourcePath = routeRegExp.exec(reqUrl)[1];
-
-
-    reqUrl = "";
-    // Serve SAPUI5 resources from cdn or local server or fs
-    if (resourceRoute.type && resourceRoute.type === "service" && resourceRoute.name === "sapui5") {
-      reqUrl = resourceRoute.version ? `/${resourceRoute.version}` : "";
-      routeProxyObject.target = oDestination.cdn;
-    } else {
-      debugger;
-      routeProxyObject.target = oDestination.URL;
-    }
-    reqUrl += resourceRoute.entryPath + resourcePath;
-
-    // Add Authorization info for proxy
-    if (oDestination.User) {
-      routeProxyObject.auth = `${oDestination.User}:${oDestination.Password}`;
-    }
-
-    return {
-      resourcePath: reqUrl,
-      proxyObject: routeProxyObject
-    };
-  }
-
-  buildRouteConfig();
-
-  return function (req, res, next) {
-      console.log(`[PROXY_SRV] ${req.url}`);
-      let sMatchedRoute = "";
-      let oMatchedRoute = {};
-      let oRouteProxy = {};
-
-      res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Allow-Credentials', 'true');
-      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-      res.header('Access-Control-Allow-Headers', 'X-Requested-With, Accept, Origin, Referer, User-Agent, Content-Type, Authorization, X-Mindflash-SessionID');
-      // intercept OPTIONS method
-      if ('OPTIONS' === req.method) {
-        res.header(200);
-        console.log(req.method + '(options): ' + req.url);
+  return async function proxyDestinations(req, res, next) {
+    try {
+      let matchedRoute;
+      
+      // Add headers for fetch
+      addHeader(res, HEADER_ALLOW_CONTROL_ALLOW_ORIGIN, "*");
+      addHeader(res, HEADER_ALLOW_CONTROL_ALLOW_CREDENTIALS, "true");
+      addHeader(res, HEADER_ALLOW_CONTROL_ALLOW_METHODS, "GET,PUT,POST,DELETE");
+      addHeader(res, HEADER_ALLOW_CONTROL_ALLOW_HEADERS, "X-Requested-With, Accept, Origin, Referer, User-Agent, Content-Type, Authorization, X-Mindflash-SessionID");
+      // Intercept OPTIONS method
+      if (req.method === "OPTIONS") {
+        res.status(200);
         next();
         return;
       }
 
-      sMatchedRoute = findRoute(req.url);
+      // Perform route matching to see if neo-app.json contains any entry matching the URL
+      matchedRoute = findRoute(req.url);
 
-      if (!sMatchedRoute || sMatchedRoute === "/") {  // If no route found, "/" is found move to next
+      if (!matchedRoute|| matchedRoute === "/") {
         next();
         return;
       }
 
-      oRouteProxy = buildProxyObject(req.url, sMatchedRoute);
-      oMatchedRoute = oRouteProxy.proxyObject;
-      req.url = oRouteProxy.resourcePath;
+      routeProxy = buildProxyObject(req.url, matchedRoute);
+      req.url = routeProxy.resourcePath;
 
-      console.log(`Redirected: ${oMatchedRoute.target}, ${req.url}`);
-      proxy.web(req, res, oMatchedRoute, (err) => {
+      proxyServer.web(req, res, routeProxy.proxyConfig, (err) => {
         if (err) {
           next(err);
         }
       });
-  }
-};
+
+    } catch (err) {
+      next(err);
+    }
+  };
+
+}
+
+module.exports = createMiddleware;
